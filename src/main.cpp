d4d3003d4cd7b5b22c2565a1beb12de7ba180210
Straight To <straighttojanestreet@rs.school>
2022-11-18 16:01:11 +0300

    wip: add some callbacks

--- src/main.cpp
+++ src/main.cpp
@@ -8,62 +8,72 @@
 #include <lexy_ext/report_error.hpp>
 
 struct Color {
-    std::uint8_t r, g, b;
+  std::uint8_t r, g, b;
 };
 
-namespace grammar
-{
+struct expression_t {
+};
+
+namespace grammar {
 namespace dsl = lexy::dsl;
 
 struct nil_literal {
-    static constexpr auto rule = LEXY_LIT("nil");
+  static constexpr auto rule = LEXY_LIT("nil");
+
+  static constexpr auto value = lexy::constant(10);
 };
 
 struct tail_literal {
-    static constexpr auto rule = LEXY_LIT("...");
+  static constexpr auto rule = LEXY_LIT("...");
+
+  static constexpr auto value = lexy::constant(20);
 };
 
 struct number_literal {
-    static constexpr auto rule = dsl::digits<>;
+  static constexpr auto rule = dsl::digits<>;
+
+  static constexpr auto value = lexy::constant(20);
 };
 
 struct bool_literal_true {
-    static constexpr auto rule = LEXY_LIT("true") | LEXY_LIT("TRUE");
+  static constexpr auto rule = LEXY_LIT("true") | LEXY_LIT("TRUE");
+
+  static constexpr auto value = lexy::constant(20);
 };
 
 struct bool_literal_false {
-    static constexpr auto rule = LEXY_LIT("false") | LEXY_LIT("FALSE");
+  static constexpr auto rule = LEXY_LIT("false") | LEXY_LIT("FALSE");
+
+  static constexpr auto value = lexy::constant(10);
 };
 
-struct bool_literal {
-    static constexpr auto rule
-        = dsl::p<::grammar::bool_literal_true>
-          | dsl::p<::grammar::bool_literal_false>;
+struct bool_literal : dsl::rule_base {
+  static constexpr auto rule
+	  = dsl::p<::grammar::bool_literal_true>
+		  | dsl::p<::grammar::bool_literal_false>;
+  static constexpr auto value = lexy::constant(10);
 };
 
 struct exp {
-    static constexpr auto rule
-        = dsl::p<::grammar::bool_literal>
-          | dsl::p<::grammar::nil_literal>
-          | dsl::p<::grammar::number_literal>;
+  static constexpr auto rule
+	  = dsl::p<::grammar::bool_literal>
+		  | dsl::p<::grammar::nil_literal>
+		  | dsl::p<::grammar::number_literal>;
+  static constexpr auto value = lexy::constant(10);
 };
 
-struct explist {
-    static constexpr auto rule
-        = dsl::p<exp>;
+struct explist : dsl::rule_base {
+  static constexpr auto rule
+	  = dsl::list(dsl::p<::grammar::exp>, dsl::sep(dsl::comma));
+
+  static constexpr auto value = lexy::forward<int8_t>;
+
 };
 } // namespace grammar
 
-int main ()
-{
-  auto input = lexy::string_input (std::string_view ("#FFFFFF"));
-  auto result = lexy::parse<grammar::color> (input, lexy_ext::report_error);
-
-  if (result.has_value ())
-    {
-      auto color = result.value ();
-      std::printf ("#%02x%02x%02x\n", color.r, color.g, color.b);
-    }
+int main() {
+  auto input = lexy::string_input(std::string_view("#FFFFFF"));
+  auto result = lexy::parse<grammar::explist>(input, lexy_ext::report_error);
 
   return result ? 0 : 1;
 }
